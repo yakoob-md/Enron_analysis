e0bcfa94727ca13520d9cd4b9bb8c2852d0500c7
yakub <dabaayakub@gmail.com>
2026-04-22 22:55:44 +0530

    bilstm changes

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ DATA_PATH = 'data/emails_labeled_silver_tenK.parquet'
 MODEL_DIR = 'models'
 RESULTS_DIR = 'results'
 
-EPOCHS = 5
-BATCH_SIZE = 16
+EPOCHS = 2
+BATCH_SIZE = 32
 MAX_LEN = 200
 VOCAB_SIZE = 10000
